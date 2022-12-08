ee49c11225690501a92e767b5739cf9d31e75993
kevinnytech <berlinstation254@gmail.com>
2022-12-08 05:20:07 -0800

    introduction to  C programing

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Rreeturn: Always O(Success)
+ */
+
+int main(void)
+{
+	puts("\Programming is like building a multilingual puzzle");
+	return (0);
+}
