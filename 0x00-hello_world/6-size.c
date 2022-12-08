2b7511f2c1189bc096a75a8954baaede897d757a
kevinnytech <berlinstation254@gmail.com>
2022-12-08 06:30:39 -0800

    introduction to  C programing

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,14 +4,19 @@
  * main - Entry point
  *
  * Desription : C program that prints the size of various types
- * Return :Always 0 (Success)
+ *
+ * Return: Always 0 (Succesful)
  */
+
 int main(void)
 {
+	/*sizeof evaluates the size of variables */
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of an double: %lu byte(s)\n", sizeof(double));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+
 	return (0);
 }
