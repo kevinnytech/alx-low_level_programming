4d32b49b7d16c4ecef9e6e7e8779ed901bd7c3e1
kevinnytech <berlinstation254@gmail.com>
2022-12-08 06:33:39 -0800

    introduction to  C programing

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,6 @@ int main(void)
 	/*sizeof evaluates the size of variables */
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
-	printf("Size of an double: %lu byte(s)\n", sizeof(double));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
