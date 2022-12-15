0c0c6f5c955960b0a9bb40d828b60fd73b1b0b64
kevinnytech <berlinstation254@gmail.com>
2022-12-15 02:48:10 -0800

    More nested loops

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,8 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -12,4 +15,5 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-#endif /* MAIN_H */ 
+
+#endif /* MAIN_H */
