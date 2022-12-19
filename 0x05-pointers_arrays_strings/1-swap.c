ad42746b5ff9a84072afa9473e49418cce11c89b
kevinnytech <berlinstation254@gmail.com>
2022-12-19 00:33:29 -0800

    1. Don't swap horses in crossing a stream

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,14 +1,18 @@
-#include <unistd.h>
 #include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * swap_int - main
+ * @a: pointer
+ * @b: pointer
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c)
+
+void swap_int(int *a, int *b)
 {
-	return (write(1, &c, 1));
+	int swap;
+
+	swap = *a;
+	*a = *b;
+	*b = swap;
 }
+
