0c05001afed66cabd8fa54b292946cbb2344e295
kevinnytech <berlinstation254@gmail.com>
2023-01-20 00:45:07 -0800

    fix error 6

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,17 +11,16 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list nums;
-	unsigned int index, sum = 0;
+	va_list ap;
+	unsigned int i, sum = 0;
 
-	va_start(nums, n);
+	va_start(ap, n);
 
-	for (index = 0; index < n; index++)
-		sum += va_arg(nums, int);
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
 
-	va_end(nums);
+	va_end(ap);
 
 	return (sum);
 }
-
 
